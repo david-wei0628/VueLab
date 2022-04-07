3efd6a557720d51d2a4f1cdfc8214ae2cf0985f4
david-wei0628 <62085333+david-wei0628@users.noreply.github.com>
2022-04-07 14:49:20 +0800

    實體掛載，SCSS

--- VueLab/wwwroot/js/Button.js
+++ VueLab/wwwroot/js/Button.js
@@ -1,12 +1,29 @@
 ﻿const vm = Vue.createApp({
-  data () {
-    return {
-      count: 0
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        plus: function () {
+            this.count++;
+        }
     }
-  },
-  methods: {
-    plus: function() {
-      this.count++;
-    }
-  }
 }).mount('#app');
+
+const vm2 = Vue.createApp({
+    data: () => {
+        return {
+            count: 0
+        }
+    },
+}).mount('#app2');
+
+const vm3 = Vue.createApp({
+    template: `<div>{{ greeting }} 好棒棒!</div>`,
+    data() {
+        return {
+            greeting: 'Hello Vue.js!'
+        }
+    }
+}).mount('#app3');
